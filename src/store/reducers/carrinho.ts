280f69e398f8d857c2adb1da5ed90d4438309b01
Vinícius Santos <vinisantoosk@gmail.com>
2024-10-20 17:59:19 -0300

    feat: redux

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Game } from '../../App'
+
+type CarrinhoState = {
+  itens: Game[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionar: (state, action: PayloadAction<Game>) => {
+      const jogo = action.payload
+      if (state.itens.find((game) => game.id === jogo.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(jogo)
+      }
+    }
+  }
+})
+export const { adicionar } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
+// function adicionarAoCarrinho(jogo: Game) {
+//     if (carrinho.find((game) => game.id === jogo.id)) {
+//       alert('Item já adicionado')
+//     } else {
+//       setCarrinho([...carrinho, jogo])
+//     }
+//   }FF
